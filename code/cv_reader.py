d4aeb7eeec6a837df92821470aae022d41258dd0
Kaartik7 <50298143+Kaartik7@users.noreply.github.com>
2021-05-08 15:32:47 +0530

    Update and rename audio_book.py to cv_reader.py

--- code/cv_reader.py
+++ code/cv_reader.py
@@ -6,7 +6,8 @@ def read_my_cv():
     """
     Program that reads my cv.
     """
-    book = open('mycv.pdf', 'rb')
+    book = open('mycv.pdf', 'rb') #name of cv is mycv, and should be present in the same directory as this py file
+   
     pdfReader = PyPDF2.PdfFileReader(book)
     for i in range(0, pdfReader.numPages):
         page = pdfReader.getPage(i)
